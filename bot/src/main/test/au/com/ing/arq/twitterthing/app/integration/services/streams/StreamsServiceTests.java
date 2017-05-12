a3187a3b4a66782ab1c33932212402e665e22128
Gabriel Viso Carrera <gvisoc@gabrimac.local>
2017-05-12 16:55:40 +1000

    Test refactor / rename

--- bot/src/main/test/au/com/ing/arq/twitterthing/app/integration/services/streams/StreamsServiceTests.java
+++ bot/src/main/test/au/com/ing/arq/twitterthing/app/integration/services/streams/StreamsServiceTests.java
@@ -3,5 +3,5 @@ package au.com.ing.arq.twitterthing.app.integration.services.streams;
 /**
  * Created by gvisoc on 12/5/17.
  */
-public class StreamsServiceImpl {
+public class StreamsServiceTests {
 }
