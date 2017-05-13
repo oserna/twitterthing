fe17768a57d0262f1a2c8e4b78052bf313077d6b
Oscar Serna <oserna@192-168-1-5.tpgi.com.au>
2017-05-13 23:21:35 +1000

    strucutre corrected

---
+++ bot/src/main/java/au/com/ing/arq/twitterthing/domain/services/event/AppendTwitterEventService.java
@@ -0,0 +1,8 @@
+package au.com.ing.arq.twitterthing.domain.services.event;
+
+/**
+ * Created by oserna on 5/13/17.
+ */
+public interface AppendTwitterEventService {
+    void appendTwitterEvent(String message);
+}
--- bot/src/test/java/au/com/ing/arq/twitterthing/app/controllers/WebHookControllerTests.java
+++ bot/src/test/java/au/com/ing/arq/twitterthing/app/controllers/WebHookControllerTests.java
@@ -4,4 +4,6 @@ package au.com.ing.arq.twitterthing.app.controllers;
  * Created by temp on 12/5/17.
  */
 public class WebHookControllerTests {
+
+
 }
